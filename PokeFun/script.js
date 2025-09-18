76689b760532d9a956bccaa60c45fb965e6b2e00
Jay <73510469+spanosjustin@users.noreply.github.com>
2025-09-18 14:17:14 -0400

    Add files via upload

--- PokeFun/script.js
+++ PokeFun/script.js
@@ -3,22 +3,42 @@ const pokePic = document.getElementById("myImg");
 
 const HIGHEST_POKEDEX_NUM = 1025;
 
+const user_pokemon = ["", "", "", "", "", ""];
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+async function getNewPokemon(pkmn) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pkmn}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+async function pokemonGenerator() {
+    let randomPokemonNum = getRandomInt(HIGHEST_POKEDEX_NUM);
+    randPokemon = await getNewPokemon(randomPokemonNum);
+    return randPokemon;
+}
+
 async function getPokemon(pkmn) {
     if(pkmn > HIGHEST_POKEDEX_NUM) {
         pokePic.style.display = "none";
         pokeInfo.innerText = "There are not that many known pokemon";
     } else {
-        const url = `https://pokeapi.co/api/v2/pokemon/${pkmn}`;
-
-        const response = await fetch(url);
-        const data = await response.json();
+        const data = await getNewPokemon(pkmn);
 
         const pokeImgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`
 
-        console.log(data);
         pokeInfo.innerText = `${data.name}`;
 
         pokePic.style.display = "block";
         pokePic.src = pokeImgURL;
+
+        const rPoke = await pokemonGenerator();
+
+        console.log(rPoke.name);
     }
 }
