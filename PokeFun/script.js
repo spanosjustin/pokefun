4efdc35854208154069999b3d24ab753d07733a8
Jay <73510469+spanosjustin@users.noreply.github.com>
2025-09-18 13:57:14 -0400

    Add files via upload

--- PokeFun/script.js
+++ PokeFun/script.js
@@ -1,11 +1,24 @@
-const api_key = "";
 const pokeInfo = document.getElementById("pokeDetails");
+const pokePic = document.getElementById("myImg");
+
+const HIGHEST_POKEDEX_NUM = 1025;
 
 async function getPokemon(pkmn) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${pkmn}`;
+    if(pkmn > HIGHEST_POKEDEX_NUM) {
+        pokePic.style.display = "none";
+        pokeInfo.innerText = "There are not that many known pokemon";
+    } else {
+        const url = `https://pokeapi.co/api/v2/pokemon/${pkmn}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+        const response = await fetch(url);
+        const data = await response.json();
 
-    pokeInfo.innerText = `${data.name}`;
+        const pokeImgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`
+
+        console.log(data);
+        pokeInfo.innerText = `${data.name}`;
+
+        pokePic.style.display = "block";
+        pokePic.src = pokeImgURL;
+    }
 }
